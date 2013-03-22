727c7bda207bc9ffa21133c146cb357c1ceeb080
Screen Flow <screenflow@GCs-MacBook-Pro.local>
2013-03-22 20:16:12 +0000

    Initial commit.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	$result = 2;
+?>
+<html>
+<head>
+	<title>Hello.com</title>
+	<link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="hero-unit">
+			<h1>Hello World</h1>
+			<p>2 + 2 = <?php echo $result; ?></p>
+		</div>
+	</div>
+</div>
+</body>
+</html>
